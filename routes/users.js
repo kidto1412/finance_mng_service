fe975f42cddb645fe5eae3c674655ffefb193972
fahriz.dimasqy <fahriz.dimasqy@dwidasa.com>
2024-08-05 22:07:34 +0700

    done database

--- routes/users.js
+++ routes/users.js
@@ -24,6 +24,7 @@ router.post("/register", async (req, res, next) => {
         email: newData.email,
         username: newData.username,
         password: hashedPassword,
+        bank: null,
       },
     });
     res.status(200).json({
