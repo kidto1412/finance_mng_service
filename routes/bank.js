1c042dfd44751490f3065653b94c9e7a618ff805
fahriz.dimasqy <fahriz.dimasqy@dwidasa.com>
2024-08-20 18:01:49 +0700

    edit bank user

--- routes/bank.js
+++ routes/bank.js
@@ -54,20 +54,101 @@ router.post("/add", async (req, res, next) => {
   }
 });
 
+router.get("/get-bank-user", async (req, res, next) => {
+  try {
+    const newData = req.query;
+
+    const existBank = await prisma.bank_user.findFirst({
+      where: {
+        bankId: parseInt(newData.bankId),
+        userId: parseInt(newData.userId),
+      },
+    });
+
+    if (!existBank) {
+      return res.status(400).json({
+        code: "01",
+        message: "Data notfound",
+      });
+    }
+
+    res.status(200).json({
+      code: "00",
+      data: existBank,
+      message: "Success",
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      code: "99",
+      data: null,
+      message: "Failed",
+    });
+  }
+});
+
 router.post("/add-bank-user", async (req, res, next) => {
   try {
     const newData = req.body;
+
+    const existBank = await prisma.bank_user.findFirst({
+      where: {
+        bankId: parseInt(newData.bankId),
+        userId: parseInt(newData.userId),
+      },
+    });
+
+    console.log(existBank);
+    // Jika sudah ada, berikan pesan error
+    if (existBank) {
+      return res.status(400).json({
+        code: "01",
+        message: "Bank already  exist",
+      });
+    }
 
     const banks = await prisma.bank_user.create({
       data: {
         userId: parseInt(newData.userId),
         bankId: parseInt(newData.bankId),
-        totalBalance: newData.totalBalance,
+        totalBalance: parseInt(newData.totalBalance),
       },
     });
     res.status(200).json({
       code: "00",
       data: "Execute",
+      message: "Success",
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      code: "99",
+      data: null,
+      message: "Failed",
+    });
+  }
+});
+
+router.put("/edit-bank-user/bankId/:bankId", async (req, res, next) => {
+  try {
+    const currentBankId = parseInt(req.params.bankId);
+    const newData = req.body;
+
+    const banks = await prisma.bank_user.update({
+      where: {
+        userId_bankId: {
+          userId: parseInt(newData.userId),
+          bankId: currentBankId,
+        },
+      },
+      data: {
+        bankId: parseInt(newData.bankId),
+        totalBalance: parseInt(newData.totalBalance),
+      },
+    });
+    res.status(200).json({
+      code: "00",
+      data: "Updated",
       message: "Success",
     });
   } catch (error) {
